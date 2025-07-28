684dd29d887936a7a3a48f9aea0b8fef87059a26
Manu <mb@datamachineintelligence.eu>
2025-07-28 16:10:26 +0530

    initial commit

---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
+import Recipe from './pages/Recipe';
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/recipe" element={<Recipe />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>,
+)
---
+++ src/pages/Recipe.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Recipe = () => {
+    return (
+      <div>
+        <h1>Recipe Detail</h1>
+      </div>
+    );
+  };
+  
+  export default Recipe;
+  
