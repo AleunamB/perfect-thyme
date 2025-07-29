128fcd8dacaa32b36bb4ff3b4b146bc1fabe6651
Manu <mb@datamachineintelligence.eu>
2025-07-29 21:03:20 +0530

    add leaf logo svg

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import leaf from "../assets/icons/leaf.svg";
 
 const Dashboard = () => {
     return (
-      <div className="bg-grey-300 h-screen w-screen">
-        <h1 className="">Perfect Thyme</h1>
+      <div className="bg-gray-400 h-screen w-screen flex justify-start pl-3 pt-3">
+        <h1 className="text-emerald-900 text-3xl">Perfect Thyme</h1>
+        <img src={leaf} alt="Leaf Logo" />
       </div>
     );
   };
